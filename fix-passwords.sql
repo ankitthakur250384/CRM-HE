81da13259c3a6d0004bc6150bbd75d9f14a4b855
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-07-06 17:54:32 +0530

    removed unnecessary files

--- fix-passwords.sql
+++
@@ -1,7 +0,0 @@
--- Fix User Passwords
--- Updates all users to have the correct bcrypt hash for password123
-
-UPDATE users SET password_hash = '$2b$10$NoAYTqBH7ojb5XUYeoe4w.mqEeCnccWbfEWQQ6erO67wJUpMwa1YS';
-
-\echo 'Password hashes updated successfully!';
-\echo 'All users now have password: password123';
